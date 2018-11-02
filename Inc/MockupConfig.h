05c70e45ab5e57fb6dfdde850e227376f0ff5830
Dimitrios Stoupis <dimitris.apple@gmail.com>
2018-11-02 07:27:56 +0000

    Have just the GPS task running, due to memory issues

--- Inc/MockupConfig.h
+++ Inc/MockupConfig.h
@@ -9,7 +9,7 @@
 #define SAT_Acq_Period_ms 			50 // The acquisition period of the satellite
 
 #define SAT_Serial_Debug 			0 // Whether to send debug data serially (for every data reception)
-#define SAT_Enable_NRF24 			1 // Whether to enable NRF24L01+
+#define SAT_Enable_NRF24 			0 // Whether to enable NRF24L01+
 #define SAT_Enable_Sensors 			0 // Whether to enable I2C sensors
 #define SAT_Enable_GPS				1 // Whether to enable GPS
 
